13043e74e2e6e8bfb2bd418a954669730476b0c5
Jmik Thang <jmikthang5@gmail.com>
2025-10-11 19:20:24 -0400

    responsive update

--- script/script.js
+++ script/script.js
@@ -1,13 +1,16 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const nav = document.querySelector('.nav');
-  const toggle = document.querySelector('.nav__toggle');
-  const menu = document.getElementById('primary-menu');
+const mobileMenu = () => {
+  const nav = document.querySelector(".nav");
+  const toggle = document.querySelector(".nav__toggle");
+  const menu = document.getElementById("primary-menu");
 
-  toggle.addEventListener('click', () => {
-    nav.classList.toggle('nav--open');
+  toggle.addEventListener("click", () => {
+    nav.classList.toggle("nav--open");
   });
 
-  menu.querySelectorAll('a').forEach(a =>
-    a.addEventListener('click', () => nav.classList.remove('nav--open'))
-  );
-});
+  menu
+    .querySelectorAll("a")
+    .forEach((a) =>
+      a.addEventListener("click", () => nav.classList.remove("nav--open"))
+    );
+};
+document.addEventListener("DOMContentLoaded", mobileMenu);
